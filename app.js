e73ff94ff8257751d8317c5750e2413557f8b20b
Eric Raio <ericraio@gmail.com>
2012-02-20 18:39:55 -0800

    init

---
+++ app.js
@@ -0,0 +1,58 @@
+var   express = require('express')
+    , app = express.createServer()
+    , io  = require('socket.io').listen(app);
+
+app.listen(8080);
+
+// routing
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/public'));
+      app.use(app.router);
+});
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+// assuming io is the Socket.IO server object
+io.configure(function () {
+    io.set('transports', ['xhr-polling']);
+    io.set('polling duration', 10);
+});
+
+// username which are currently connected to the chat
+var usernames = {};
+
+io.sockets.on('connection', function (socket) {
+
+  // when the client emits 'sendchat', this listens and executes
+  socket.on('sendchat', function (data) {
+    // we tell the client to execute 'updatechat' with 2 parameters
+    io.sockets.emit('updatechat', socket.username, data);
+  });
+
+  // when the client emits 'adduser', this listens and executes
+  socket.on('adduser', function(username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    // echo to client they've connected
+    socket.emit('updatechat', 'SERVER', 'you have connected');
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+    // update the list of users in chat, client-side
+    io.sockets.emit('updateusers', usernames);
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    delete usernames[socket.username];
+    // update list of users in chat, client-side
+    io.sockets.emit('updateusers', usernames);
+    // echo globally that this client has left
+    socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+  });
+});
---
+++ public/javascripts/application.js
@@ -0,0 +1,39 @@
+var socket = io.connect('http://localhost:8080');
+
+//on connection to server, ask for user's name with an anonmyous callback
+socket.on('connect', function() {
+  //call the server-side function 'adduser' and send one parameter (value of prompt)
+  socket.emit('adduser', prompt("What's your name?"));
+});
+
+// listener, whenever the server emits 'updatechat', this updates the chat body
+socket.on('updatechat', function(username, data) {
+  $('#conversation').append('<b>' + username + ':</b> ' + data + '<br>');
+});
+
+// listener, whenever the server emits 'updateusers', this updates the username list
+socket.on('updateusers', function(data) {
+  $('#users').empty();
+  $.each(data, function(key, value) {
+    $('#users').append('<div>' + key + '</div>');
+  });
+});
+
+// on load of page
+$(function() {
+  // when the client clicks SEND
+  $('#datasend').click(function() {
+    var message = $('#data').val();
+    $('#data').val('');
+    // tell the server to execute 'sendchat' and send along one parameter
+    socket.emit('sendchat', message);
+  });
+
+  // when the client hits ENTER on their keyboard
+  $('#data').keypress(function(e) {
+    if(e.which == 13) {
+      $(this).blur();
+      $('#datasend').focus().click();
+    }
+  });
+});
