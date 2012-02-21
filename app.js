fc527ba37ba47e79d5bc0fccb128db4d8a000d61
Eric Raio <ericraio@gmail.com>
2012-02-20 18:52:24 -0800

    fixing silly syntax error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var   express = require('express')
     , app = express.createServer()
-    , io  = require('socket.io').listen(app);
+    , io  = require('socket.io').listen(app)
     , port = process.env.PORT || 3000;
 
 app.listen(port);
